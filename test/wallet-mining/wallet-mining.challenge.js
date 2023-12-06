1f38e12924a1b91966c1f2966ba6876b1ed8ed61
root <libaice147@gmail.com>
2023-12-06 05:14:39 +0000

    add wallet mining puzzle update

--- test/wallet-mining/wallet-mining.challenge.js
+++ test/wallet-mining/wallet-mining.challenge.js
@@ -5,13 +5,13 @@ describe('[Challenge] Wallet mining', function () {
     let deployer, player;
     let token, authorizer, walletDeployer;
     let initialWalletDeployerTokenBalance;
-    
+
     const DEPOSIT_ADDRESS = '0x9b6fb606a9f5789444c17768c6dfcf2f83563801';
     const DEPOSIT_TOKEN_AMOUNT = 20000000n * 10n ** 18n;
 
     before(async function () {
         /** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */
-        [ deployer, ward, player ] = await ethers.getSigners();
+        [deployer, ward, player] = await ethers.getSigners();
 
         // Deploy Damn Valuable Token contract
         token = await (await ethers.getContractFactory('DamnValuableToken', deployer)).deploy();
@@ -19,10 +19,10 @@ describe('[Challenge] Wallet mining', function () {
         // Deploy authorizer with the corresponding proxy
         authorizer = await upgrades.deployProxy(
             await ethers.getContractFactory('AuthorizerUpgradeable', deployer),
-            [ [ ward.address ], [ DEPOSIT_ADDRESS ] ], // initialization data
+            [[ward.address], [DEPOSIT_ADDRESS]], // initialization data
             { kind: 'uups', initializer: 'init' }
         );
-        
+
         expect(await authorizer.owner()).to.eq(deployer.address);
         expect(await authorizer.can(ward.address, DEPOSIT_ADDRESS)).to.be.true;
         expect(await authorizer.can(player.address, DEPOSIT_ADDRESS)).to.be.false;
@@ -33,7 +33,7 @@ describe('[Challenge] Wallet mining', function () {
         );
         expect(await walletDeployer.chief()).to.eq(deployer.address);
         expect(await walletDeployer.gem()).to.eq(token.address);
-        
+
         // Set Authorizer in Safe Deployer
         await walletDeployer.rule(authorizer.address);
         expect(await walletDeployer.mom()).to.eq(authorizer.address);
@@ -65,7 +65,37 @@ describe('[Challenge] Wallet mining', function () {
     });
 
     it('Execution', async function () {
-        /** CODE YOUR SOLUTION HERE */
+        let addr;
+
+        for (let i = 1; i < 100; i++) {
+            addr = ethers.utils.getContractAddress({
+                from: "",
+                nonce: i,
+            });
+
+            if (addr == "0x9B6fb606A9f5789444c17768c6dFCF2f83563801") {
+                console.log("Deposit target address", addr, "recreated");
+                console.log("Deposit deployment nonce", i);
+            } else if (addr == "0x34CfAC646f301356fAa8B21e94227e3583Fe3F5F") {
+                console.log("Copy target address", addr, "recreated");
+                console.log("Copy deployment nonce", i);
+            }
+        }
+
+        for (let i = 0; i < 100; i++) {
+            addr = ethers.utils.getContractAddress({
+              from: "0x1aa7451DD11b8cb16AC089ED7fE05eFa00100A6A",
+              nonce: i,
+            });
+            if (addr == "0x76E2cFc1F5Fa8F6a5b3fC4c8F4788F0116861F9B") {
+              console.log("MockFacotry target address", addr, "recreated");
+              console.log("MockFacotry deployment nonce", i);
+            } else if (addr == "0x34CfAC646f301356fAa8B21e94227e3583Fe3F5F") {
+              console.log("Master target address", addr, "recreated");
+              console.log("Master deployment nonce", i);
+            }
+          }
+
     });
 
     after(async function () {
@@ -85,7 +115,7 @@ describe('[Challenge] Wallet mining', function () {
         expect(
             await ethers.provider.getCode(DEPOSIT_ADDRESS)
         ).to.not.eq('0x');
-        
+
         // The deposit address and the Safe Deployer contract must not hold tokens
         expect(
             await token.balanceOf(DEPOSIT_ADDRESS)
@@ -97,6 +127,6 @@ describe('[Challenge] Wallet mining', function () {
         // Player must own all tokens
         expect(
             await token.balanceOf(player.address)
-        ).to.eq(initialWalletDeployerTokenBalance.add(DEPOSIT_TOKEN_AMOUNT)); 
+        ).to.eq(initialWalletDeployerTokenBalance.add(DEPOSIT_TOKEN_AMOUNT));
     });
 });
